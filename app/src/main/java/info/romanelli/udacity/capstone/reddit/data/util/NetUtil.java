05f37ee695247665b138324105f6087cd95473c3
aromanelli <adrian.romanelli@gmail.com>
2018-10-27 18:35:32 -0700

    Refactored method registerForNetworkMonitoring, and added javadocs.

--- app/src/main/java/info/romanelli/udacity/capstone/reddit/data/util/NetUtil.java
+++ app/src/main/java/info/romanelli/udacity/capstone/reddit/data/util/NetUtil.java
@@ -19,6 +19,8 @@ public class NetUtil {
 
     final static private String TAG = NetUtil.class.getSimpleName();
 
+    final static public String INTENT_ACTION_CONNECTIVITY_CHANGE = "connectivity_change";
+
     static private boolean CONNECTED = false;
     static private ConnectivityManager.NetworkCallback NETCALLBACK;
 
@@ -37,18 +39,37 @@ public class NetUtil {
         throw new IllegalStateException("Not yet registered for network monitoring!");
     }
 
+    /**
+     * <p>Once this method has been called, you can call {@link #isConnected()} to
+     * determine if a valid connection to the Net exists or not.</p><br>
+     *
+     * <p>Also, when connectivity changes, a {@link Context#sendBroadcast(Intent)}
+     * will be done, and the {@link Intent} will have an {@link Intent#getAction()} value
+     * of {@link NetUtil#INTENT_ACTION_CONNECTIVITY_CHANGE}, and an extra {@link Boolean}
+     * value that can be obtained via the {@link Intent#getBooleanExtra(String, boolean)}
+     * key value of {@link ConnectivityManager#EXTRA_NO_CONNECTIVITY}.</p>
+     *
+     * @param context Will be used to obtain the app context via {@link Context#getApplicationContext()}
+     * @throws IllegalStateException if {@link Context#getSystemService(String)} returns
+     *      {@code null} (which it should not)
+     */
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     synchronized static public void registerForNetworkMonitoring(final Context context) throws IllegalStateException {
 
         if (NETCALLBACK == null) {
 
+            final Context appContext = context.getApplicationContext();
+
             // Configure initial network state ...
             ConnectivityManager cm =
-                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            if (cm != null) {
-                NetworkInfo netInfo = cm.getActiveNetworkInfo();
-                CONNECTED = (netInfo != null) && netInfo.isConnected();
+                    (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            if (cm == null) {
+                throw new IllegalStateException("Unable to obtain the CONNECTIVITY_SERVICE system service!");
             }
+
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            CONNECTED = (netInfo != null) && netInfo.isConnected();
 
             // Configure callback/listener for network state changes ...
             NETCALLBACK = new ConnectivityManager.NetworkCallback() {
@@ -57,31 +78,27 @@ public class NetUtil {
                     Log.d(TAG, "onAvailable() called with: network = [" + network + "]");
                     // https://developer.android.com/reference/android/net/ConnectivityManager.NetworkCallback#onAvailable(android.net.Network)
                     CONNECTED = true;
-                    context.getApplicationContext().sendBroadcast(getIntent());
+                    appContext.sendBroadcast(getIntent());
                 }
                 @Override
                 public void onLost(Network network) {
                     Log.d(TAG, "onLost() called with: network = [" + network + "]");
                     CONNECTED = false;
-                    context.getApplicationContext().sendBroadcast(getIntent());
+                    appContext.sendBroadcast(getIntent());
                 }
                 private Intent getIntent() {
                     Intent intent = new Intent();
-                    intent.setAction(context.getApplicationContext().getPackageName() + "CONNECTIVITY_CHANGE");
+                    intent.setAction(INTENT_ACTION_CONNECTIVITY_CHANGE);
                     intent.putExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, (!CONNECTED));
                     return intent;
                 }
             };
 
             // Regiseter callback/listener for network state changes ...
-            ConnectivityManager connMgr =
-                    (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-            if (connMgr != null) {
-                connMgr.registerNetworkCallback(new NetworkRequest.Builder().build(), NETCALLBACK);
-            }
+            cm.registerNetworkCallback(new NetworkRequest.Builder().build(), NETCALLBACK);
 
         } else {
-            throw new IllegalStateException("Already registered for network monitoring!");
+            Log.d(TAG, "registerForNetworkMonitoring: Already registered for network monitoring");
         }
 
     }
