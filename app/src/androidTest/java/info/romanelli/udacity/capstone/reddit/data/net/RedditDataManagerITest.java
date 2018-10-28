f232a3db21ba9be496f6a8b9219eaa25b4da65dc
aromanelli <adrian.romanelli@gmail.com>
2018-10-27 19:56:46 -0700

    Implemented class RedditDataManager, responsible for fetching all new posts for all subscribed subreddits, and returning that data to the listener passed into it.

---
+++ app/src/androidTest/java/info/romanelli/udacity/capstone/reddit/data/net/RedditDataManagerITest.java
@@ -0,0 +1,84 @@
+package info.romanelli.udacity.capstone.reddit.data.net;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.util.Log;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import info.romanelli.udacity.capstone.TestUtil;
+import info.romanelli.udacity.capstone.reddit.data.net.newposts.model.NewPostData;
+import info.romanelli.udacity.capstone.reddit.data.net.oauth.RedditAuthManager;
+import info.romanelli.udacity.capstone.reddit.data.util.NetUtil;
+
+import static org.junit.Assert.assertEquals;
+
+public class RedditDataManagerITest {
+
+    final static private String TAG = RedditDataManagerITest.class.getSimpleName();
+
+    @Test
+    public void fetchData() {
+
+        TestUtil.setNetworkAccess(true);
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("info.romanelli.udacity.capstone", appContext.getPackageName());
+
+        NetUtil.registerForNetworkMonitoring(appContext);
+        Assert.assertTrue(NetUtil.isConnected());
+
+        // If below fails, try running LauncherActivity to authorize app by user!
+        Assert.assertTrue(RedditAuthManager.isAuthorized(appContext));
+
+        final AtomicBoolean done = new AtomicBoolean(false);
+        try {
+
+            RedditDataManager.getRedditData(appContext, new RedditDataManager.Listener() {
+                private int counter;
+                private int size;
+                @Override
+                public void fetching(int size) {
+                    this.size = size;
+                }
+                @Override
+                public void fetched(List<NewPostData> listNewPostData) {
+                    Log.d(TAG, "fetched:Thread: " + Thread.currentThread().getName());
+                    counter++;
+                    if (counter >= size) {
+                        done.set(true);
+                    }
+                    // TODO Too arbitrary of a test value, always assumes at least one new post in subreddit!
+                    Assert.assertTrue(listNewPostData.size() > 0);
+                }
+                @Override
+                public void failed(Throwable t) {
+                    done.set(true);
+                    Assert.fail(t.getLocalizedMessage());
+                }
+            });
+
+            int waitCounter = 0;
+            while (!done.get()) {
+                Thread.sleep(250);
+                waitCounter++;
+                if (waitCounter % 4 == 0) {
+                    Log.d(TAG, "fetchData: Waiting for completion ... ["+ (waitCounter / 4) +"]");
+                }
+                if (waitCounter > 2400) { // 250 * 2400 = 600,000ms == 60 seconds
+                    Assert.fail("Interrupted while waiting for completion!");
+                    break;
+                }
+            }
+
+        } catch (Throwable t) {
+            Assert.fail(t.getLocalizedMessage());
+        }
+
+    }
+
+}
---
+++ app/src/main/java/info/romanelli/udacity/capstone/reddit/data/net/RedditDataManager.java
@@ -0,0 +1,139 @@
+package info.romanelli.udacity.capstone.reddit.data.net;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import info.romanelli.udacity.capstone.reddit.data.net.newposts.NewPostsFetcher;
+import info.romanelli.udacity.capstone.reddit.data.net.newposts.model.NewPostData;
+import info.romanelli.udacity.capstone.reddit.data.net.newposts.model.NewPosts;
+import info.romanelli.udacity.capstone.reddit.data.net.oauth.RedditAuthManager;
+import info.romanelli.udacity.capstone.reddit.data.net.subreddits.SubredditsFetcher;
+import info.romanelli.udacity.capstone.reddit.data.net.subreddits.model.SubredditData;
+import info.romanelli.udacity.capstone.reddit.data.net.subreddits.model.Subreddits;
+import info.romanelli.udacity.capstone.reddit.data.util.AppExecutors;
+import info.romanelli.udacity.capstone.reddit.data.util.NetUtil;
+
+public class RedditDataManager {
+
+    final static private String TAG = RedditDataManager.class.getSimpleName();
+
+    /**
+     * <p>Will move the network calls off to another thread for you.</p>
+     * @param context
+     * @param listener See {@link RedditDataManager.Listener}
+     */
+    static public void getRedditData(final Context context, final Listener listener) {
+        Log.d(TAG, "getRedditData:Thread: " + Thread.currentThread().getName());
+
+        final Context appContext = context.getApplicationContext();
+
+        NetUtil.registerForNetworkMonitoring(appContext);
+        if (!NetUtil.isConnected()) {
+            listener.failed(new IllegalStateException("Expected a valid net connection."));
+        }
+
+        if (!RedditAuthManager.isAuthorized(appContext)) {
+            listener.failed(new IllegalStateException("Expected a valid Reddit authorization."));
+        }
+
+        fetchSubreddits(appContext, listener);
+    }
+
+    static private void fetchSubreddits(final Context appContext, final Listener listener) {
+        AppExecutors.$().netIO().execute(() -> {
+            Log.d(TAG, "fetchSubreddits:Thread: " + Thread.currentThread().getName());
+            SubredditsFetcher.fetchSubscribed(
+                    appContext,
+                    new SubredditsFetcher.Listener() {
+                        @Override
+                        public void fetched(Subreddits subreddits) {
+                            // Gets called on the 'Main' thread, so we
+                            // move back off to the 'Net I/O' thread ...
+                            AppExecutors.$().netIO().execute(() -> {
+                                final List<SubredditData> subredditData = new ArrayList<>();
+                                if (subreddits != null &&
+                                        subreddits.getSubredditsData() != null &&
+                                        subreddits.getSubredditsData().getSubreddits() != null) {
+                                    subreddits.getSubredditsData().getSubreddits().forEach(subreddit -> {
+                                        if (subreddit.getSubredditData() != null) {
+                                            subredditData.add(subreddit.getSubredditData());
+                                        }
+                                    });
+                                }
+                                fetchNewPosts(appContext, subredditData, listener);
+                            });
+                        }
+
+                        @Override
+                        public void failed(Throwable t) {
+                            Log.e(TAG, "failed: ", t);
+                            listener.failed(t);
+                        }
+                    }
+            );
+        });
+    }
+
+    static private void fetchNewPosts(final Context appContext,
+                                      final List<SubredditData> subredditDataList,
+                                      final Listener listener) {
+        Log.d(TAG, "fetchNewPosts:Thread: " + Thread.currentThread().getName());
+        listener.fetching(subredditDataList.size());
+        subredditDataList.forEach(
+                subredditData -> {
+                    Log.d(TAG, "fetchNewPosts|SubredditData: [" + subredditData.getTitle() + "][" + subredditData.getDisplayName() + "][" + subredditData.getUrl() + "]");
+                    NewPostsFetcher.fetchNewPosts(
+                            subredditData.getDisplayName(),
+                            appContext,
+                            new NewPostsFetcher.Listener() {
+                                @Override
+                                public void fetched(NewPosts newposts) {
+                                    final List<NewPostData> listNewPostsData = new ArrayList<>();
+                                    if (newposts != null &&
+                                            newposts.getNewPostsData() != null &&
+                                            newposts.getNewPostsData().getNewPosts() != null) {
+                                        newposts.getNewPostsData().getNewPosts().forEach(newpost -> {
+                                            if (newpost.getNewPostData() != null) {
+                                                Log.d(TAG, "fetchNewPosts|NewPostData: [" + newpost.getNewPostData().getSubreddit() + "][" + newpost.getNewPostData().getTitle() + "]");
+                                                listNewPostsData.add(newpost.getNewPostData());
+                                            }
+                                        });
+                                    }
+                                    listener.fetched(listNewPostsData);
+                                }
+                                @Override
+                                public void failed(Throwable t) {
+                                    Log.e(TAG, "failed: ", t);
+                                    listener.failed(t);
+                                }
+                            }
+                    );
+                }
+        );
+    }
+
+    public interface Listener {
+        /**
+         * <p>Called when the number of times {@link #fetched(List)}
+         * will be called is known, before the calls happen.</p>
+         * @param size
+         */
+        void fetching(int size);
+
+        /**
+         * <p>Will be called on the 'main' thread!</p>
+         * @param listNewPostData
+         */
+        void fetched(final List<NewPostData> listNewPostData);
+
+        /**
+         * <p>Called when an error occurs on any of the fetches.</p>
+         * @param t
+         */
+        void failed(final Throwable t);
+    }
+
+}
