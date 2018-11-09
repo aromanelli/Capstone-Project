afd1ac5ec86eb99cf079ac1b57dea766fe98259b
aromanelli <adrian.romanelli@gmail.com>
2018-11-08 16:35:16 -0800

    Added @Contract annotation to the Assert.that(...) methods, to help AS to properly NOT mark 'can be null' statements that follow the Assert.that call.

--- app/src/main/java/info/romanelli/udacity/capstone/util/Assert.java
+++ app/src/main/java/info/romanelli/udacity/capstone/util/Assert.java
@@ -2,18 +2,39 @@ package info.romanelli.udacity.capstone.util;
 
 import android.util.Log;
 
+import org.jetbrains.annotations.Contract;
+
 import java.util.Arrays;
 
+/**
+ * <p>
+ *     A class that replaces the java keyword {@code assert}, which does not work
+ *     well (at all?) in the Android environment.
+ * </p><br>
+ * <p>
+ *     Add "{@code implementation 'org.jetbrains:annotations:16.0.3'}" (changing
+ *     the version number to match the latest version) to the build.gradle file,
+ *     as the annotations library is not on the classpath by default.
+ * </p><br>
+ * @see <a href="https://blog.jetbrains.com/idea/2013/10/better-control-flow-analysis-with-contract-annotations-and-intellij-idea-13/">Better Control Flow Analysis with Contract Annotations and IntelliJ</a>
+ * @see <a href="https://mvnrepository.com/artifact/org.jetbrains/annotations">JetBrains Java Annotations</a>
+ * @author Adrian O. Romanelli
+ * @version 1.0
+ */
 public class Assert {
 
     public static final String TAG = Assert.class.getSimpleName();
 
-    // TODO figure out why I can't get the 'assert' keyword to work correctly!
+    /*
+    implementation 'org.jetbrains:annotations:16.0.3'
+     */
 
+    @Contract("false -> fail")
     public static void that(boolean condition) throws AssertionError {
         Assert.that(condition, null);
     }
 
+    @Contract("false, _ -> fail")
     public static void that(boolean condition, String message) throws AssertionError {
         if (!condition) {
             AssertionError ae;
