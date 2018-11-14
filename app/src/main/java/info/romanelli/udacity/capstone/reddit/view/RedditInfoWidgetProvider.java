9c87f0d224875708e7d3173ad4866caef9a01571
aromanelli <adrian.romanelli@gmail.com>
2018-11-14 01:28:51 -0800

    Added missing contentDescription tags, per reviewer comment.

--- app/src/main/java/info/romanelli/udacity/capstone/reddit/view/RedditInfoWidgetProvider.java
+++ app/src/main/java/info/romanelli/udacity/capstone/reddit/view/RedditInfoWidgetProvider.java
@@ -112,20 +112,5 @@ public class RedditInfoWidgetProvider extends AppWidgetProvider {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
     }
 
-//    @Override
-//    public void onEnabled(Context context) {
-//        // Enter relevant functionality for when the first widget is created
-//    }
-
-//    @Override
-//    public void onDisabled(Context context) {
-//        // Enter relevant functionality for when the last widget is disabled
-//    }
-
-//    @Override
-//    public void onDeleted(Context context, int[] appWidgetIds) {
-//        // Perform any action when one or more AppWidget instances have been deleted
-//    }
-
 }
 
